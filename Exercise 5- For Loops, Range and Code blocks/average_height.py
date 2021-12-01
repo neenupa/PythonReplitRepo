705180589640304d95db4e755c95d6133a107b17
NeenuPaul <94364171+neenupa@users.noreply.github.com>
2021-12-01 11:08:03 +0000

    5

---
+++ Exercise 5- For Loops, Range and Code blocks/average_height.py
@@ -0,0 +1,26 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+
+print(student_heights)
+
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+# Without FOR LOOP
+
+# avg_height= round(sum(student_heights)/len(student_heights))
+# print(f"Average height = {avg_height}")
+
+#With FOR LOOP
+total_height=0
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+  total_height += student_heights[n]
+
+avg_height=round(total_height/len(student_heights))
+print(f"Average height = {avg_height}")
+
