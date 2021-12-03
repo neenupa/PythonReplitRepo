05721b9512f298e15bb4e1a9d285856f73012102
NeenuPaul <94364171+neenupa@users.noreply.github.com>
2021-12-03 07:04:52 +0000

    6

---
+++ Exercise 6- Functions, Code blocks and While loop/reeborg-robot-challenge.py
@@ -0,0 +1,21 @@
+#Robot Challenge
+# Link :
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+def turn_right():
+ turn_left()
+ turn_left()
+ turn_left()
+
+def jump():
+ move()
+ turn_left()
+ move()
+ turn_right()
+ move()
+ turn_right()
+ move()
+ turn_left()
+
+for i in range(0, 6):
+ jump()
